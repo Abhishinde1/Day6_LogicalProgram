022366d7a1d0c0ffa454a442dd020351513b896d
AbhiShinde1 <abhishinde572@gmail.com>
2022-08-31 16:34:16 +0530

    [Abhishek] FibonacciSeries_Program

---
+++ FibonacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic
+{
+    internal class FibonacciSeries
+    {
+        
+            public static void Fibonacci()
+            {
+                Console.WriteLine("Please enter how many fibonocci numbers we have to generate");
+                int number = Convert.ToInt32(Console.ReadLine());
+                int firstNum = 0, second = 1;
+                int result = 0;
+                result = firstNum + second;
+                Console.Write("{0} ", firstNum );
+                for (int i = 1; i < number; i++)
+                {
+                firstNum = second;
+                    second = result;
+                    result = firstNum + second;
+                    Console.Write(" " + result);
+                }
+            }
+        }
+    }
+
+
