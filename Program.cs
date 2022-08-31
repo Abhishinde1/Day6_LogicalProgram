bdf04009953ce26af9acf581bd4eb9b783b0c77f
AbhiShinde1 <abhishinde572@gmail.com>
2022-08-31 17:46:43 +0530

    Welcome to Logical Program

--- Program.cs
+++ Program.cs
@@ -6,12 +6,21 @@
         {
             Console.WriteLine("WelCome to LogicalProgram");
 
-            Console.WriteLine("Choose option \n 1.Fibonacci Series");
+            Console.WriteLine("Choose option \n 1.Fibonacci Series  \n 2:Perfect Number \n 3: Prime Number \n 4:Reverse Number");
             int optionCheck = Convert.ToInt32(Console.ReadLine());
             switch (optionCheck)
             {
                 case 1:
                     FibonacciSeries.Fibonacci();
+                    break;
+                case 2:
+                    UC2Perfect_Number.Perfectnumber();
+                    break;
+                case 3:
+                    UC3Prime_NUM.PrimeNumber();
+                    break;
+                case 4:
+                    UC4Reverse_Number.ReverseNumber();
                     break;
             }
 
