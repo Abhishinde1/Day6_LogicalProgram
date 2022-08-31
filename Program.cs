b4bce9c87c0d848acb8f4864468173543fd75b51
AbhiShinde1 <abhishinde572@gmail.com>
2022-08-31 16:31:47 +0530

    [Abhishek] Welcome to Logical Program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace logic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to LogicalProgram");
+
+            Console.WriteLine("Choose option \n 1.Fibonacci Series");
+            int optionCheck = Convert.ToInt32(Console.ReadLine());
+            switch (optionCheck)
+            {
+                case 1:
+                    FibonacciSeries.Fibonacci();
+                    break;
+            }
+
+        }
+    }
+}
+    
